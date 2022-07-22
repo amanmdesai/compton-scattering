fc508bf4790feacac1559862bebb89b05019f234
amanmdesai <amanmukeshdesai@gmail.com>
2022-07-22 16:01:23 +0530

    add file

--- MC-compton.py
+++ MC-compton.py
@@ -9,16 +9,18 @@ import matplotlib.pyplot as plt
 pi = math.pi
 m = 0.000510 # mass of electron in gev  
 alpha = 1/132.507 # alpha
-E = .7 # initial photon energy in MeV
+E = .1 # initial photon energy in MeV
 E = E*1E-3 # initial photon energy in Gev 
 delta = 2.0
+w = E #(E - m**2)/(2*m)
+w = w/m
 convert = 3.894E8 # GeV^-2 to pb
 
 def dsigma(costh):
-    pre_fact = pi*alpha*alpha/(m**2)
-    pre_fact *= (1+(E*(1-costh)/m))**2
-    sum_1 = (1+(E*(1-costh)/m))
-    sum_1 += E* (1-costh)/m
+    pre_fact = pi*(alpha/m)**2
+    pre_fact *= 1/(1 +  w*(1-costh))**2
+    sum_1 = 1/(1 + w*(1-costh))
+    sum_1 += w*(1-costh)
     sum_1 += costh**2
     return pre_fact*sum_1
 
@@ -72,26 +74,22 @@ def gen_events(Nevent,w_max):
 
     while i < Nevent:
         costh = -1 + random.random()*delta 
-        costh = -1 + random.random()*delta 
         w_ii =  dsigma(costh) * delta
         prob = w_ii/w_max
         random_num = random.random()
         if random_num<prob: 
             m_costh[i] = math.degrees(math.acos(costh))
-            #print(m_costh[i])
             sinth = math.sqrt(1 - costh**2)
-            ph_e[i] = E/(1+((E/m)*(1-costh))) 
-            ph_px[i]  = ph_e[i] * sinth 
-            ph_py[i] = 0 
+            ph_e[i]  = m*E/(E*(1-costh) + m)   #E/(1+((E/m)*(1-costh))) 
+            ph_px[i] = 0
+            ph_py[i] = ph_e[i] * sinth 
             ph_pz[i] = ph_e[i] * costh 
 
 
-            el_px[i]  = - ph_px[i] 
-            el_py[i] = 0 
+            el_px[i] = - ph_px[i] 
+            el_py[i] = - ph_py[i] 
             el_pz[i] = E - (ph_e[i] * costh) #math.sqrt(el_e[i]**2 - m**2  - el_px[i]**2)
-            el_e[i] = math.sqrt(m**2 + el_px[i]**2 + el_pz[i]**2)
-            #sum_[i] = ph_e[i] + el_e[i]
-            #print(sum_[i])
+            el_e[i]  = E - ph_e[i] # math.sqrt(m**2 + el_px[i]**2 + el_py[i]**2 + el_pz[i]**2)
             i = i + 1
 
     plot_photon(ph_px,"Px")
@@ -111,8 +109,8 @@ def gen_events(Nevent,w_max):
 
 
 def main():
-    N = 1000000
-    Nevent = 100000
+    N = 100000
+    Nevent = 10000
     w_sum = 0
     w_max = 0
     w_square = 0
@@ -125,11 +123,20 @@ def main():
     variance = math.sqrt(w_square/N - (w_sum/N)**2)
     gen_events(Nevent,w_max)
     sigma_x = w_sum*convert/(N*1E12)
+    print("Number of Events ", Nevent)
+    print("Energy  ", E*1000, "MeV")
     print("Effective Luminsoity ", Nevent/sigma_x, "barn^-1")
     print("cross section in barn ", sigma_x, " barn")
     print("error in cross section in barn ", variance*convert/(math.sqrt(N)*1E12), " barn")
-    #plt.figure()
-    #plt.hist(costh)
-    #plt.show()
+
 
 main()
+
+
+'''
+pre_fact = pi*alpha*alpha/(m**2)
+pre_fact *= (1+(E*(1-costh)/m))**2
+sum_1 = (1+(E*(1-costh)/m))
+sum_1 += E * (1-costh)/m
+sum_1 += costh**2
+'''
